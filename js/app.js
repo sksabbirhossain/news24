ffddaa0440d2886a228b0b9edde47a61b75932c2
sksabbirhossain <sksabbirhossain4@gmail.ocm>
2022-09-03 00:01:50 +0600

    showing category dynamically

--- js/app.js
+++ js/app.js
@@ -1,1 +1,19 @@
-console.log("hello world");
+// categories
+const categories = () => {
+  fetch("https://openapi.programming-hero.com/api/news/categories")
+    .then((res) => res.json())
+    .then((data) => {
+      //   console.log(data.data);
+      const categories = data.data.news_category;
+      categories.forEach((category) => {
+        console.log(category);
+        const categoryItems = document.getElementById("category-items");
+        const li = document.createElement("li");
+        li.classList.add("pe-2");
+        li.innerHTML = `<a href="#" onclick="newsByCategory('${category.category_id}')">${category.category_name}</a>`;
+        categoryItems.appendChild(li);
+      });
+    });
+};
+
+categories();
